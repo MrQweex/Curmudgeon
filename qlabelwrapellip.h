8e6f701227be0f2333a1c5edfe7ef54b0d467dd2
MrQweex <MrQweex@qweex.com>
2013-08-12 01:47:51 -0600

    Tweaking the button wrap

--- qlabelwrapellip.h
+++ qlabelwrapellip.h
@@ -19,20 +19,35 @@ private:
         QFontMetrics metrics(font());
         int currentChar = 0, w=0, h=1;
         int lastX = -1;
+
+        // Iterate over length of original name
         while(currentChar<ename.length())
         {
+            // Find the maximum width of characters that can fit on the line
             w = 0;
+            std::cout << "  width starts at " << currentChar << std::endl;
             while(w<maxWidth && currentChar<ename.length())
             {
                 currentChar++;
                 w+=metrics.width(ename[currentChar]);
             }
-            w-=metrics.width(ename[currentChar]);
-            currentChar--;
+            w-=metrics.width(ename[currentChar--]);   // We went over by 1, so subtract
+            std::cout << "c=" << currentChar << " w=" << w << std::endl;
+            std::cout << ename.mid(lastX+1, currentChar).toStdString() << std::endl;
 
-            int x = ename.lastIndexOf(" ", currentChar);
+            if(currentChar>=ename.length()-1)
+                break;
+
+            // Find the preceding whitespace
+            int x;
+            if(ename[currentChar+1].toLatin1()==' ')
+                x = currentChar+1;
+            else
+                x = ename.lastIndexOf(" ", currentChar);
+            // Make sure this whitespace is not from the last line
             if(x<=lastX || x==-1)
             {
+                // Insert a dash mid-word
                 w+=metrics.width(ename[currentChar]);
                 ename = ename.insert(currentChar,"-");
                 x = currentChar+1;
@@ -40,10 +55,10 @@ private:
                 //    x=++currentChar;
             }
 
-            if(currentChar==ename.length()-1)
+            if(currentChar>=ename.length()-1)
                 break;
 
-            h++;
+            h++;    // Number of lines
             if(h*metrics.height()>maxHeight)
             {
                 ename = ename.left(currentChar);
@@ -59,6 +74,7 @@ private:
             }
             ename = ename.replace(x,1,'\n');
             lastX = x;
+            currentChar = x;
         }
         return ename;
     }
@@ -90,8 +106,8 @@ public:
 
         int left, right, top, bottom;
         parentWidget()->getContentsMargins(&left, &top, &right, &bottom);
-        maxWidth = this->size().width();
-        maxHeight = this->size().height();
+        maxWidth = this->size().width() - this->margin();
+        maxHeight = this->size().height() - this->margin();
 
         QLabel::setText(getShortText(fullText));
     }
